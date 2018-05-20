225af7c9d639ef9037a1528f32564761080d4d80
julie98 <37369872+julie98@users.noreply.github.com>
2018-05-20 17:18:47 +0800

    Add files via upload

---
+++ chapter_5/age_test.py
@@ -0,0 +1,14 @@
+age = 65
+
+if age < 2:
+	print("He is an infant.")
+elif age < 4:
+	print("He is learning to walk.")
+elif age < 13:
+	print("He is a child.")
+elif age < 20:
+	print("He is a teenager.")
+elif age < 65:
+	print("He is a grown-up.")
+elif age >= 65:
+	print("He is a senior.")
