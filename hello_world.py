b88b65b252203bb81d10bc1ae3cb384e7834ae40
julie98 <37369872+julie98@users.noreply.github.com>
2018-05-20 16:59:47 +0800

    chapter_2

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+print("Hello python world!")
+
+message = "Hello python crash course world!"
+print(message)
+
+message = "George is the cutest dog in the world!"
+print(message)
---
+++ name_strip.py
@@ -0,0 +1,6 @@
+name = "   \n\t Albert Eistein  \n"
+
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
